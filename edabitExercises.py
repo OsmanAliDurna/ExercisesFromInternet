02f589c988741e4aea4ca89ed9d0083f81b176e0
Osman Ali Durna <o_durna@hotmail.com>
2021-02-21 16:06:14 +0200

    edabit exercises

---
+++ edabitExercises.py
@@ -0,0 +1,46 @@
+# After the equals = sign, enter the string "edabit.com".
+# Remember that strings must be wrapped in quotes " ".
+
+website = "edabit.com"
+
+# Let's create a variable from scratch.
+# Declare the variable food.
+# Assign it the value "pizza".
+
+food = "pizza"
+
+# Declare a variable named day and assign it the value 19.
+
+day = 19
+
+# Divide 100 by 2.
+
+division = 100//2
+
+# Use the + operator to concatenate (combine) two strings together.
+# Declare the variable name.
+# Concatenate first_name with last_name.
+
+first_name = "Luke "
+last_name = "Skywalker"
+name = first_name + last_name
+
+# The below function takes two parameters, each containing a number.
+# Multiply the first parameter num1 by the second parameter num2.
+# Remember to use the return keyword, as shown in the above example.
+
+def multiply_two_numbers(num1, num2):
+    return num1*num2
+
+# Return the element "Donatello".
+# Remember the first element in a list is 0.
+# Always return your answer!
+
+turtles = ["Raphael", "Michelangelo", "Leonardo", "Donatello"]
+
+def turtle_power(turtles):
+    return turtles[3]
+
+# Given a list of numbers, set the value of the data stored at index 1 to 88.
+
+numbers = [1, 4, 6, 8, 0]
