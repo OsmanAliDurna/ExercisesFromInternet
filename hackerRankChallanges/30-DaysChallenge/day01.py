eecc8871df665027d13dd3a746e3e5e9f1e769ef
Osman Ali Durna <o_durna@hotmail.com>
2021-02-24 21:32:46 +0200

    correction

--- hackerRankChallanges/30-DaysChallenge/day01.py
+++ hackerRankChallanges/30-DaysChallenge/day01.py
@@ -15,22 +15,19 @@ You can switch languages using the drop-down menu above your editor, and the inp
 the best-practice conventions of your submission language.
 
 Input Format
-
 A single line of text denoting inputString (the variable whose contents must be printed).
 
 Output Format
-
 Print Hello, World. on the first line, and the contents of  on the second line.
 
 Sample Input
-
 Welcome to 30 Days of Code!
-Sample Output
 
+Sample Output
 Hello, World. 
 Welcome to 30 Days of Code!
-Explanation
 
+Explanation
 On the first line, we print the string literal Hello, World.. On the second line, we print the contents of the  variable which,
 for this sample case, happens to be Welcome to 30 Days of Code!. If you do not print the variable's contents to stdout, you will not pass the hidden test case.
 """
