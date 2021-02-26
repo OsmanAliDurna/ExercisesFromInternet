6a460c972d4cd4c14a3b07518fafebe4fdc9f767
Osman Ali Durna <o_durna@hotmail.com>
2021-02-26 22:19:42 +0200

    little modified

--- Exercises/guessingNumberGame.py
+++ Exercises/guessingNumberGame.py
@@ -5,17 +5,19 @@ answer = random.randint(1, 101)
 max = 7
 counter = 0
 
-question = 'I can find it in max {} times : '.format(max)
+question = 'I try to find it in max {} times : '.format(max)
 print ("Let's play the guessing game!")
 
 while True:
   guess = int(input(question))
+  counter += 1
   if guess < answer:
-    counter += 1
     print('Little higher, I have still ', (max - counter), 'chances.')
   elif guess > answer:
-    counter += 1
     print('Little lower, I have still ', (max - counter), 'chances.')
   else:
     print('As I said I find it in ',counter,' times')
+    break
+  if (counter > 6):
+    print("I LOSE this time :'( ")
     break
