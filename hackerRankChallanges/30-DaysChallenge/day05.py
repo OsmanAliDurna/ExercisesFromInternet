954263fbee55bc37deefe097ef335f61afbc2070
Osman Ali Durna <o_durna@hotmail.com>
2021-03-01 03:31:18 +0200

    HackerRankDay05

--- hackerRankChallanges/30-DaysChallenge/day05.py
+++ hackerRankChallanges/30-DaysChallenge/day05.py
@@ -1,106 +1,62 @@
 """
 Objective
-In this challenge, we're going to learn about the difference between a class and an instance; because this is an Object Oriented concept,
-it's only enabled in certain languages. Check out the Tutorial tab for learning materials and an instructional video!
+In this challenge, we will use loops to do some math. Check out the Tutorial tab to learn more.
 
 Task
-Write a Person class with an instance variable, age, and a constructor that takes an integer, initialAge, as a parameter.
-The constructor must assign initialAge to age after confirming the argument passed as initialAge is not negative;
-if a negative argument is passed as initialAge, the constructor should set age to 0 and print Age is not valid, setting age to 0.
-In addition, you must write the following instance methods:
+Given an integer, n, print its first 10 multiples. Each multiple n * i (where 1 <= i <= 10) should be printed on a new line in the form: n * i = result.
 
-1. yearPasses() should increase the age instance variable by 1.
-2. amIOld() should perform the following conditional actions:
-    If age < 13, print You are young..
-    If age >= 13 and age < 18, print You are a teenager..
-    Otherwise, print You are old..
+Example
+n = 3
+The printout should look like this:
 
-To help you learn by example and complete this challenge, much of the code is provided for you, but you'll be writing everything in the future.
-The code that creates each instance of your Person class is in the main method. Don't worry if you don't understand it all quite yet!
-
-Note: Do not remove or alter the stub code in the editor.
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
 
 Input Format
-Input is handled for you by the stub code in the editor.
-The first line contains an integer, T (the number of test cases), and the T subsequent lines each contain an integer denoting the age of a Person instance.
+A single integer, n.
 
 Constraints
-1 <= T <= 4
--5 <= age <= 30
+2 <= n <= 20
 
 Output Format
-Complete the method definitions provided in the editor so they meet the specifications outlined above; the code to test your work is already in the editor.
-If your methods are implemented correctly, each test case will print 2 or 3 lines (depending on whether or not a valid initialAge was passed to the constructor).
+
+Print 10 lines of output; each line i (where 1 <= i <= 10) contains the result of n * i in the form:
+n * i = result.
 
 Sample Input
-4
--1
-10
-16
-18
+2
 
 Sample Output
-Age is not valid, setting age to 0.
-You are young.
-You are young.
-
-You are young.
-You are a teenager.
-
-You are a teenager.
-You are old.
-
-You are old.
-You are old.
-
-Explanation
-Test Case 0: initialAge = -1
-Because initialAge < 0, our code must set age to 0 and print the "Age is not valid..." message followed by the young message.
-Three years pass and age = 3, so we print the young message again.
-
-Test Case 1: initialAge = 10
-Because initialAge < 13, our code should print that the person is young. Three years pass and age = 13, so we print that the person is now a teenager.
-
-Test Case 2: initialAge = 16
-Because 13 <= initialAge < 18 , our code should print that the person is a teenager. Three years pass and age = 19, so we print that the person is old.
-
-Test Case 3: initialAge = 18
-Because initialAge >= 18, our code should print that the person is old. Three years pass and the person is still old at age = 21, so we print the old message again.
-
-The extra line at the end of the output is supposed to be there and is trimmed before being compared against the test case's expected output.
-If you're failing this challenge, check your logic and review your print statements for spelling errors.
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
 """
 
-class Person:
-    def __init__(self,initialAge):
-        # Add some more code to run some checks on initialAge
-        if (initialAge < 0):
-            print("Age is not valid, setting age to 0.")
-            initialAge = 0
-        return
-    
-    def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
-        if ( 0 <=  age ) and ( age < 13 ):
-            print("You are young.")
-        
-        elif ( 13 <=  age ) and ( age < 18 ):
-            print("You are teenager.")
-        
-        elif (18 <= age):
-            print("You are old.")
-        return
+#!/bin/python3
 
-    def yearPasses(self):
-        # Increment the age of the person in here
-        return 
+import math
+import os
+import random
+import re
+import sys
 
-t = int(input())
-for i in range(0, t):
-    age = int(input())         
-    p = Person(age)  
-    p.amIOld()
-    for j in range(0, 3):
-        p.yearPasses()       
-    p.amIOld()
-    print("")
+if __name__ == '__main__':
+    n = int(input())
+    if (n >= 2) and (n <= 20):
+        for i in range(1,11):
+            print("{} x {} = {}".format(n, i, n*i))
