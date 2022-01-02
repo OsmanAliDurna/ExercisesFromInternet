fa0b2bcf64aa92be3553af2a0875f06b26767025
Osman Ali Durna <o_durna@hotmail.com>
2022-01-02 23:02:53 +0100

    Validating and Parsing Email Addresses started

--- hackerRankChallanges/Exercises/renameForHR.py
+++ hackerRankChallanges/Exercises/renameForHR.py
@@ -1,4 +1,4 @@
-text = """Validating phone numbers"""
+text = """Validating and Parsing Email Addresses"""
 
 print("hackerRank" +
       text.title().replace(" ", "").replace('"', "").replace("?", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
