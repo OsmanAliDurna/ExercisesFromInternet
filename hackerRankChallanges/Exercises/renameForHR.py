62e0908c3060fc819a76f6447874d69f0a9854b6
Osman Ali Durna <o_durna@hotmail.com>
2022-01-02 22:25:09 +0100

    Validating phone numbers solved

--- hackerRankChallanges/Exercises/renameForHR.py
+++ hackerRankChallanges/Exercises/renameForHR.py
@@ -1,4 +1,4 @@
-text = """Validating Roman Numerals"""
+text = """Validating phone numbers"""
 
 print("hackerRank" +
       text.title().replace(" ", "").replace('"', "").replace("?", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
