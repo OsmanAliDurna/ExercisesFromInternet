e40850b4b2f01a13ed5540f6fd8c8c4d82787cde
Osman Ali Durna <o_durna@hotmail.com>
2022-01-11 23:59:17 +0100

    Combinations needs last review

--- hackerRankChallanges/Exercises/hackerRankItertoolsCombinations.py
+++ hackerRankChallanges/Exercises/hackerRankItertoolsCombinations.py
@@ -6,10 +6,11 @@ the combination tuples will be produced in sorted order.
 
 Sample Code:
 >>> from itertools import combinations
->>> 
+>>>
 >>> print list(combinations('12345',2))
-[('1', '2'), ('1', '3'), ('1', '4'), ('1', '5'), ('2', '3'), ('2', '4'), ('2', '5'), ('3', '4'), ('3', '5'), ('4', '5')]
->>> 
+[('1', '2'), ('1', '3'), ('1', '4'), ('1', '5'), ('2', '3'),
+  ('2', '4'), ('2', '5'), ('3', '4'), ('3', '5'), ('4', '5')]
+>>>
 >>> A = [1,1,3,3,3]
 >>> print list(combinations(A,4))
 [(1, 1, 3, 3), (1, 1, 3, 3), (1, 1, 3, 3), (1, 3, 3, 3), (1, 3, 3, 3)]
@@ -47,8 +48,15 @@ HK
 from itertools import combinations
 
 text, size = input().upper().split()
+output = []
 
-for i in range(len(list(combinations(str(text), int(size))))):
-    for j in range(int(size)):
-        print(sorted(list(combinations(str(text), int(size))))[i][j], end="")
+for i in range(int(size)):
+    container = []
+    for j in range(len(list(combinations(str(text), i + 1)))):
+        container.append((list(combinations(str(text), i + 1)))[j])
+    output += sorted(set(container))
+
+for k in range(len(output)):
+    for l in range(len(output[k])):
+        print(output[k][l], end="")
     print()
