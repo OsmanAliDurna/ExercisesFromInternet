b947b4b65d500b8b770f2013cf91bc191a94e118
Osman Ali Durna <o_durna@hotmail.com>
2022-01-19 18:29:59 +0100

    oop classses

---
+++ Django/19.01/oop_classes.py
@@ -0,0 +1,26 @@
+class Person:
+    name = "Barry"
+    age = 44
+
+
+person1 = Person()
+person2 = Person()
+
+# ekstradan attribute ekledik.
+
+# print(person1.name)
+# print(person2.name)
+
+# Person.job = "teacher"
+
+# print(person1.job)
+# print(person2.job)
+
+person1.name = "Aaron"
+
+print(person1.name)
+print(person2.name)
+
+person1.lastname = "Ak"
+
+print(person1.lastname)
