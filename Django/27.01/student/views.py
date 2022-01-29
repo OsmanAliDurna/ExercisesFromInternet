da5d69163129eac4cc063382249c5238c215bfad
Osman Ali Durna <o_durna@hotmail.com>
2022-01-29 11:34:14 +0100

    django crud

---
+++ Django/27.01/student/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'student/index.html')
+
+
+def student_page(request):
+    form = StudentForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'student/student.html', context)
